e161bd33d195a77e58ab272f26b8d7a229590549
Whovian79 <andrew.walsh001@student.swic.edu>
2020-02-05 13:46:13 -0600

    Global variables vs. Object Keys

--- scripts.js
+++ scripts.js
@@ -1,36 +1,15 @@
 //25 February 2020
 
-//UNDEFINED is a PRIMITIVE TYPE.
-// It signifies the absence of ANY value.
-//let x;
+//Global variables vs. Object Keys
+const x = 23;
+const name = "fake";
 
-//x =23
-
-//NULL is a PRIMITIVE TYPE.
-//NULL represents a deliberate assignment of `nothing`
-let x = null;
-console.log(x);
-
-//this a JS OBJECT.(Use brackets to creat an object.)
 const me = {
-  name: "AJ"
+  name: "Manav"
 };
 
-console.log(me.name); //DOT NOTATION
+//Reference the GLOBAL VARIABLE
+console.log(name);
 
-//Create a new key value pair with age = 23.
-me.age = 23;
-me.name = "Ian";
-
-console.log(me);
-
-//NESTED OBJECT
-const me = {
-  name: "AJ",
-  age: 40,
-  pets: {
-      cat: "Luna",
-      dog: "Panda"
-  }
-};
-console.log(me.pets);
+//Reference the key `name` inside of the OBJECT ME.
+console.log(me.name);
