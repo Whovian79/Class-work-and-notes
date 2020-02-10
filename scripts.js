1b20ecda392dfa8029d74d9d855cc874809bd488
Whovian79 <andrew.walsh001@student.swic.edu>
2020-02-10 13:46:27 -0600

    bracket notaion and expressions

--- scripts.js
+++ scripts.js
@@ -1,15 +1,38 @@
-//25 February 2020
+//!10 February 2020!
 
-//Global variables vs. Object Keys
-const x = 23;
-const name = "fake";
+//Use BRACKET NOTATION to access a key that is REFERENCED by a VARIABLE.
+//console.log(`Bracket notation with variable:${me[keyThatINeed]}`);
 
-const me = {
-  name: "Manav"
-};
+//console.log(me["keyThatINeed"]); //undefined
+// Relational or COMPARISON OPERATORS always yields BOOLEANS.
+console.log(1 === 2);
 
-//Reference the GLOBAL VARIABLE
-console.log(name);
+//To check if 2 things are considered to equal...
+//`===` is STRICT EQUALITY
+//It checks the VALUE and TYPE.
+console.log(2 === "2"); //false
+console.log(2 === 3);
+console.log(2 !== 3); //true
 
-//Reference the key `name` inside of the OBJECT ME.
-console.log(me.name);
+//EXPRESSION is anything that evaluates to a value
+//STATEMENTS combine EXPRESSIONS with keywords and other SYNTAX.
+const x = 3;
+
+if (x === 3) {
+  //{}s establish SCOPE of the code.
+  console.log("x is 3!");
+} else {
+  console.log("x is not 3")
+}
+
+if (age >= 25){
+  console.log("Renting a car is affordable");
+} else if (age >= 18){
+  //If age is less than or equal to 24
+  console.log("Renting will be expensive!")
+} else {//All other ages
+console.log {
+  ("You can't rent a car legally!")
+}
+}
+
