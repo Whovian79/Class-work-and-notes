bd37dd01791eff3cfaf480486a3599b999e8e39d
Whovian79 <andrew.walsh001@student.swic.edu>
2020-02-17 13:15:41 -0600

    more comparison

--- scripts.js
+++ scripts.js
@@ -1,38 +1,14 @@
-//!10 February 2020!
-
-//Use BRACKET NOTATION to access a key that is REFERENCED by a VARIABLE.
-//console.log(`Bracket notation with variable:${me[keyThatINeed]}`);
-
-//console.log(me["keyThatINeed"]); //undefined
-// Relational or COMPARISON OPERATORS always yields BOOLEANS.
-console.log(1 === 2);
-
-//To check if 2 things are considered to equal...
-//`===` is STRICT EQUALITY
-//It checks the VALUE and TYPE.
-console.log(2 === "2"); //false
-console.log(2 === 3);
-console.log(2 !== 3); //true
-
-//EXPRESSION is anything that evaluates to a value
-//STATEMENTS combine EXPRESSIONS with keywords and other SYNTAX.
+// ! 17 Feb 2020!
 const x = 3;
 
-if (x === 3) {
-  //{}s establish SCOPE of the code.
-  console.log("x is 3!");
-} else {
-  console.log("x is not 3")
+// TODO: Check if x is ODD AND if it's less than 10.
+if (x % 2 === 1 && x < 10);
+{
+  console.log("x is odd because it has a remainder of 1.");
 }
 
-if (age >= 25){
-  console.log("Renting a car is affordable");
-} else if (age >= 18){
-  //If age is less than or equal to 24
-  console.log("Renting will be expensive!")
-} else {//All other ages
-console.log {
-  ("You can't rent a car legally!")
+// TODO: Check if x is ODD  OR  if it's less than 10.
+if (x % 2 === 1 || x < 10);
+{
+  console.log("x is odd because it has a remainder of 1.");
 }
-}
-
